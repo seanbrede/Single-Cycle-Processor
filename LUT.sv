92cc156dba15cea45b9a9be9d9f2bf508cb16354
Sean Brede <sabrede@ucsd.edu>
2021-02-12 14:48:21 -0800

    Add files via upload

--- LUT.sv
+++ LUT.sv
@@ -1,19 +1,23 @@
-// CSE141L
-// possible lookup table for PC target
-// leverage a few-bit pointer to a wider number
-// Lookup table acts like a function: here Target = f(Addr);
-//  in general, Output = f(Input); 
-module LUT(
-  input       [ 1:0] Addr,
-  output logic[ 9:0] Target
-  );
+// the lookup table acts like a function: target = f(index);
+module LUT (
+	input        [4:0] index,
+	output logic [7:0] target
+	);
 
 always_comb 
-  case(Addr)		   //-16'd30;
-	2'b00:   Target = 10'h3ff;  // -1
-	2'b01:	 Target = 10'h003;
-	2'b10:	 Target = 10'h007;
-	default: Target = 10'h001;
-  endcase
+	case (index)
+		5'd01:   target = 8'd000; // convenience
+		5'd02:   target = 8'd001; // convenience
+		5'd03:   target = 8'd009; // iterating through tap patterns
+		5'd04:   target = 8'd010; // bounding the number of spaces
+		5'd05:   target = 8'd026; // bounding the number of spaces
+		5'd06:   target = 8'd054; // 
+		5'd07:   target = 8'd061; // 
+		5'd08:   target = 8'd064; // 
+		5'd09:   target = 8'd065; // 
+		5'd10:   target = 8'd075; //
+		// more constants to probably go here
+		default: target = 8'd255; // error: tried to look up a nonexistent index
+	endcase
 
 endmodule
