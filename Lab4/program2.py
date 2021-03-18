282577d1f69edda3e0142f85e4c48ac03e12a655
Thomas <thomas.j.stuart96@gmail.com>
2021-03-17 21:09:28 -0700

    cleaned up program 2

--- Lab4/program2.py
+++ Lab4/program2.py
@@ -1,6 +1,5 @@
 import compile_helpers as chs; MEM = chs.initMemory1()
-
-
+# *** DEBUG ONLY ****
 f = open('./Tests/p2t60s1.txt', "r")
 a = []
 for line in f:
@@ -11,7 +10,7 @@ for line in f:
 f.close()
 for i in range(len(a)):
     MEM[64+i] = a[i]
-
+# *** DEBUG ONLY ****
 
 # labels are all caps as var name with string values  JUMP and HERE
 # TAP_SELECT = "HERE"
@@ -38,15 +37,16 @@ parity          = 0             # r14
 # r15
 # cannot h
 
-
 def cycle_LFSR( LFSR_st, tap):
     x       = LFSR_st & tap
     new_bit =  chs.redXOR(x)
+############# *** DEBUG ONLY **** ###################  b/c verilog shift left will clear the last bit
     # GET MSB
     MSB     = (LFSR_st >> 6) & 1
     MSB = MSB << 6
     # CLEAR MSB
     LFSR_st  = LFSR_st ^ MSB
+############# *** DEBUG ONLY **** ###################
     # SHIFT LEFT
     LFSR_st   = LFSR_st << 1
     nextState = LFSR_st | new_bit
@@ -55,7 +55,6 @@ def cycle_LFSR( LFSR_st, tap):
 # 1. Figure out the tap pattern
 while found == 0:
     curr_tap = chs.tap_LUT[tap_select]
-    # print('curr_tap = ', hex(curr_tap))
     lfsr_st  = lfsr_st_init
     read_ptr = 65  # start at the space after the seed value
     last_ptr = 74  # read up until the last space value
@@ -64,40 +63,37 @@ while found == 0:
     while read_ptr < last_ptr:
         parity          = MEM[read_ptr] & 128
         echar_no_parity = MEM[read_ptr] ^ parity
+        # compute expected lfsr and lfsr with the selected tap
+        expected_state   = echar_no_parity ^ 32          # what we should be get if its the correct LSFR
+        lfsr_st          = cycle_LFSR(lfsr_st, curr_tap) # what the current tap pattern produces
 
-        expected_state   = echar_no_parity ^ 32          # predicted state
-        lfsr_st          = cycle_LFSR(lfsr_st, curr_tap) # actual state
-
-        # actual    != predicted, go to next tap
+        # actual    != expected, go to next tap
         if lfsr_st != expected_state:
-            # print('\t failure @ ', read_ptr)
-            # print('\t expected state ', hex(expected_state))
-            # print('\t lfsr_st ', hex(lfsr_st))
             tap_select += 1
             break
-        # else:
-        #     print('\t success @ ', read_ptr, " LFSR = ", hex(lfsr_st))
 
         if read_ptr == last_ptr - 1:
             found = 1
-
+        # read in the next MEM value and continue checking
         read_ptr += 1
 
 print('tap selection done.  tap selected -> ', hex(curr_tap) )
+
+# 2.  RESET READ POINTER
 read_ptr = 64
 
-# # 2.  decode the message by iterating through
-# WRITE = "HERE"
+# 3.  decode the message by iterating through
 while write_ptr < write_end:
     #  get rid of the parity bit
     parity          = MEM[read_ptr] & 128
     echar_no_parity = MEM[read_ptr] ^ parity
+
     # decrypt character
     if read_ptr == 64:
         lfsr_st        = echar_no_parity ^ 32
         MEM[write_ptr] = 32  # since we know the frist 10 chars are a space, we can fill the first one like so
 
-    if read_ptr != 64:
+    if 64 < read_ptr:
         MEM[write_ptr] = lfsr_st ^ echar_no_parity
 
     # cycle the LFSR
@@ -113,5 +109,5 @@ s = ""
 for i in range(64):
     v = int(MEM[i])
     s += chr(v)
-
 print(s)
+# *** DEBUG ONLY ****
